bf3323fa7256c93e779b2f00a8a5a872f3ebbc92
thjungers <thomasjungers@gmail.com>
2024-04-17 18:45:09 +0200

    Generate a markdown list of links to calendars

---
+++ calendar_links.md
@@ -0,0 +1,19 @@
+# Ans
+
+[PDF source](https://www.intradel.be/trier-ses-dechets/mon-calendrier-des-collectes.htm?commune=2)
+
+[Ans (Zone 1)](https://github.com/thjungers/intradel-calendar/releases/latest/download/Ans_zone-1.ics)
+
+[Ans (Zone 2)](https://github.com/thjungers/intradel-calendar/releases/latest/download/Ans_zone-2.ics)
+
+[Ans (Zone 3)](https://github.com/thjungers/intradel-calendar/releases/latest/download/Ans_zone-3.ics)
+
+# Liege
+
+[Liege (Lundi)](https://github.com/thjungers/intradel-calendar/releases/latest/download/Liege_lundi.ics) ([PDF source](https://www.intradel.be/trier-ses-dechets/mon-calendrier-des-collectes.htm?commune=39))
+
+# Theux
+
+[PDF source](https://www.intradel.be/trier-ses-dechets/mon-calendrier-des-collectes.htm?commune=65)
+
+[Theux](https://github.com/thjungers/intradel-calendar/releases/latest/download/Theux.ics)
--- scripts/generate_ics.py
+++ scripts/generate_ics.py
@@ -7,16 +7,49 @@ import arrow
 import ics
 
 from json_calendar_parser import JsonCalendarParser
+from utils import DEFAULT_ZONES_KEYWORD
 
 from pprint import pprint
 
 ICS_OUTPUT = Path("ics/")
 ICS_OUTPUT.mkdir(exist_ok=True)
+GITHUB_PROJECT_URL = "https://github.com/thjungers/intradel-calendar"
+
+def make_markdown_links(municipality, cal_paths, parser):
+    """Make the markdown source for the links file, for a given municipality."""
+    urls = parser.data["url"]
+
+    links = [f"# {municipality}"]
+    if not isinstance(urls, dict):
+        # single URL
+        links.append(f"[PDF source]({urls})")
+    
+    for zone, path in cal_paths:
+        txt = municipality
+        if zone != DEFAULT_ZONES_KEYWORD:
+            txt += f" ({zone})"
+
+        md = f"[{txt}]({GITHUB_PROJECT_URL}/releases/latest/download/{path.name})"
+
+        if isinstance(urls, dict):
+            # multiple URLs
+            md += f" ([PDF source]({urls[zone]}))"
+
+        links.append(md)
+
+    return links
+
 
 def main():
+    links_md = []
+
     for json_file in Path("json").glob("*.json"):
         parser = JsonCalendarParser(json_file)
-        parser.save_ics_calendars(ICS_OUTPUT / json_file.stem)
+        cal_paths = parser.save_ics_calendars(ICS_OUTPUT / json_file.stem)
+        links_md.extend(make_markdown_links(json_file.stem, cal_paths, parser))
+
+    with Path("calendar_links.md").open("w") as file:
+        file.write("\n\n".join(links_md))
 
 
 if __name__ == "__main__":
