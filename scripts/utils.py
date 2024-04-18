68bd1a2b70db2133af2f80ee381d159094eb62a5
thjungers <thomasjungers@gmail.com>
2024-04-18 20:28:19 +0200

    Extract part of changelog for CI pipeline

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,6 +2,7 @@
 
 from datetime import date, timedelta
 from pathlib import Path
+import re
 import arrow
 
 ONEDAY = timedelta(days=1)
@@ -39,3 +40,32 @@ def make_ics_path(path: Path, zone: str) -> Path:
             path.name + "_" + zone.replace(" ", "-").lower()
         )
     return path.with_suffix(".ics")
+
+def get_changelog_version(version_tag: str) -> str:
+    """Extract the changelog for the given version tag from CHANGELOG.md.
+    
+    Parameters:
+        version_tag: The version tag, optionnaly prepended by "refs/tags/".
+    """
+    version = re.match("^(?:/refs/tags/)?(v.*)$", version_tag).group(1) # type: ignore
+
+    root = Path(__file__).parent.parent
+
+    changelog = []
+
+    with (root / "CHANGELOG.md").open("r") as file:
+        while line := file.readline():
+            if line.strip() == f"# {version}":
+                break
+        else:
+            raise ValueError(f"Section for version {version} not found in CHANGELOG.md")
+        
+        while line := file.readline():
+            if line.startswith("# "):
+                break
+
+            line = line.strip()
+            if line:
+                changelog.append(line)
+
+    return "\n".join(changelog)
